7b06dfe87d0a22d8d45470c6656b32fa86812f7a
steve chesnowitz <steve@chesnowitz.com>
2016-05-13 01:58:09 +0000

    merging branch

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
   devise_for :users
   resources :statuses
   root to: 'statuses#index'
-
+    # Making Friends ---------------------
+  resources :users, only: [:index, :show]    
+   resources :friendships, only: [:create, :destroy, :accept] do 
+    member do 
+      put :accept
+    end
+  end
+    # Making Friends ---------------------
   
 end
