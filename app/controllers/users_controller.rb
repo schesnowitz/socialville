a2dba372567facf259d40c9ca0ffe3e1a48c4092
steve chesnowitz <steve@chesnowitz.com>
2016-05-13 00:02:01 +0000

    merging branch

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,17 +2,19 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_the_user, only: [:show]
   
+# Making Friends ---------------------  
 	def index
-    	case params[:people] when "friends"
-          @users = current_user.active_friends
-        when "requests"
-          @users = current_user.pending_friend_requests_from.map(&:user)
-        when "pending"
-          @users = current_user.pending_friend_requests_to.map(&:friend)
-        else
-          @users = User.where.not(id: current_user.id)
-        end
+	case params[:people] when "friends"
+      @users = current_user.active_friends
+    when "requests"
+      @users = current_user.pending_friend_requests_from.map(&:user)
+    when "pending"
+      @users = current_user.pending_friend_requests_to.map(&:friend)
+    else
+      @users = User.where.not(id: current_user.id)
+    end
 	end
+# Making Friends ---------------------	
 
   def show
   end
