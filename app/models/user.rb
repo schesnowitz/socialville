5c019bdac7f44e97bdbf3f344c4c1d962d4e7906
steve chesnowitz <steve@chesnowitz.com>
2016-05-13 03:34:38 +0000

    creating friends section

--- app/models/user.rb
+++ app/models/user.rb
@@ -56,6 +56,10 @@ class User < ApplicationRecord
       end
     end
   end
+  
+  def friendship_relation(user_2)
+    Friendship.where(user_id: [self.id,user_2.id], friend_id: [self.id,user_2.id]).first
+  end
   # Making Friends ---------------------
   
 end
